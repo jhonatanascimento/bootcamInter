f365de26c03401aa8bda4c97cae63dc22b10a01b
jhonatanascimento <jhonata_ti@outlook.com>
2022-01-11 08:59:42 -0300

    conditional render with &&

--- aulasReact/introducao-react/src/App.jsx
+++ aulasReact/introducao-react/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 
 
+const buttonA = <button>Historico de Clientes</button>
+const buttonB = <button>Segundo Botão</button>
+const hasCustomer = false;
+
+
 function App(){
     return(
-        <><div>Hello World</div>
-        <div>WOW</div></>
+        <div>
+         <h1>Hello World</h1>
+         {hasCustomer &&(
+             <div>
+                 Clique no botão abaixo para visualizar o historico dos Clientes 
+                 <br/>
+                 {buttonA}
+             </div>
+         )}    
+        </div>
+         
     )
 
 }
